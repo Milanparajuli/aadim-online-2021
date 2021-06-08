f2416c3e2d86382e947d5558137c531059781eff
Yuba Raj Kalathoki <yubaraj.kalathoki@zurelsoft.com>
2021-06-08 07:30:26 +0545

    Maile online class demo ko lagi helloworld code add gareko

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+This is testing comment on my hello world class.
+*/
+public class HelloWorld {
+/**
+This is the main method which is required to print hello world
+*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
