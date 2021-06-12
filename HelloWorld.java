bef62958c717ad3d0e180169a1a19dc07ee85899
Milanparajuli <80202149+Milanparajuli@users.noreply.github.com>
2021-06-12 20:15:56 +0545

    adding name

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,7 @@ This is the main method which is required to print hello world
 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("Milan Parajuli");
+
 	}
 }
